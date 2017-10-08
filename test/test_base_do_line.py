368dedb1a1aad0d3ad20860b018b2a60bbafc757
Daniel Knuettel <daknuett@gmail.com>
2017-10-08 11:52:44 +0200

    added initial tests

---
+++ test/test_base_do_line.py
@@ -0,0 +1,24 @@
+import nf.base.do.line as line
+import numpy as np
+import sympy, math
+
+def test_integrate_line_rect():
+	f = lambda x: x**2
+
+	X = sympy.symbols("X")
+	term = X**2
+
+	sympy.integrate(term)
+
+	r = np.arange(0, 10.0, 0.001)
+	class StepIterator(object):
+		def __init__(self, step):
+			self.step = step
+		def __iter__(self):
+			return self
+		def __next__(self):
+			return self.step
+
+	assert math.isclose(line.integrate_numbers_rect(iter(r), StepIterator(0.001), f, 0),
+		sympy.integrate(term, 0, 10))
+		
