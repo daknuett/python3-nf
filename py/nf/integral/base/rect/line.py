e03da55eeb4c3a43d22fcd447289e016130528ea
Daniel Knuettel <daknuett@gmail.com>
2017-10-09 13:39:05 +0200

    added nf.integrate.base.rect.integrate_width

--- py/nf/integral/base/rect/line.py
+++ py/nf/integral/base/rect/line.py
@@ -10,7 +10,7 @@ class StepIterator(object):
 		return self.width
 
 
-def integrate(f, rnge, steps):
+def integrate_steps(f, rnge, steps):
 	"""
 	Approximate the integral of ``f``
 	in the range ``rnge`` using a decomposition
@@ -35,5 +35,31 @@ def integrate(f, rnge, steps):
 
 
 	width = (stop - start) / steps
-	return integrate_numbers_rect(iter(arange(start, stop, width)), StepIterator(width), 0)
+	return integrate_numbers_rect(iter(arange(start, stop, width)), StepIterator(width), f, 0)
+
+def integrate_width(f, rnge, width):
+	"""
+	Approximate the integral of ``f``
+	in the range ``rnge`` using a decomposition
+	with subintervalls with ``width`` length. 
+
+	``f`` must return ``float`` or ``int``.
+	``rnge`` must be ``range`` or a ``tuple`` containing
+	``start`` and ``stop``
+
+	Algorithm: *Riemann upper sum*
+	"""
+
+	if(isinstance(rnge, range)):
+		start = rnge.start
+		stop = rnge.stop
+	elif(isinstance(rnge, list, tuple)):
+		if(len(rnge) != 2):
+			raise ValueError("rnge must have length 2: (start, stop)")
+		start, stop = rnge
+	else:
+		raise TypeError("rnge must be range or list or tuple")
+
+
+	return integrate_numbers_rect(iter(arange(start, stop, width)), StepIterator(width), f, 0)
 
